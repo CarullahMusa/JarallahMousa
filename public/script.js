6fccb91b91a7a8602c554718b86337562b882c01
CarullahMusa <caruniversite@gmail.com>
2024-06-09 22:56:09 +0300

    Update İndex html
    Update Nav
    Add animation

--- public/script.js
+++ public/script.js
@@ -1,25 +1,81 @@
-// const hide = document.querySelector("#navbar-cta");
 
-// const clickBtn = document.querySelector("button").addEventListener("click", function(){
-//     hide.remove("hidden");
+const sideBar = document.querySelector('#sideNav');
+
+function hiddenBar() {
+    if (window.innerWidth > 768) {
+        sideBar.classList.add("hidden");
+    } else {
+        sideBar.classList.remove("hidden");
+    }
+}
+
+window.addEventListener('resize', hiddenBar);
+
+hiddenBar();
+
+
+function openNav() {
+    document.body.classList.toggle("overflow-hidden");
+    document.getElementById("sideBar").style.width = "100%";
+    document.getElementById("sideNav").style.width = "60%";
+}
+
+/*Close navigation*/
+function exitNavB() {
+    document.body.classList.toggle("overflow-hidden");
+    document.getElementById("sideBar").style.width = "0";
+    document.getElementById("sideNav").style.width = "0";
+}
+
+const liElements = document.querySelectorAll('.m-li');
+
+liElements.forEach((li) => {
+    li.addEventListener('click', (event) => {
+        // Diğer tüm <li> elemanlarının arka plan rengini sıfırla
+        liElements.forEach((otherLi) => {
+            otherLi.classList.remove("bg-green-100");
+        });
+
+        // Tıklanan elemanın arka plan rengini değiştir
+        event.currentTarget.classList.toggle("bg-green-100");
+    });
+});
+
+const navliElements = document.querySelectorAll('.nav-li');
+
+navliElements.forEach((li) => {
+    li.addEventListener('click', (event) => {
+        // Diğer tüm <li> elemanlarının arka plan rengini sıfırla
+        navliElements.forEach((otherLi) => {
+            otherLi.classList.replace("text-green-500", "text-green-900");
+        });
+
+        // Tıklanan elemanın arka plan rengini değiştir
+        event.currentTarget.classList.replace("text-green-900", "text-green-500");
+    });
+});
+
+// const navLinks = document.querySelectorAll('.nav-li');
+
+// navLinks.forEach((link) => {
+//   const sectionId = link.getAttribute('href').replace('#', '');
+//   const section = document.getElementById(sectionId);
+
+//   window.addEventListener('scroll', () => {
+//     if (isElementInViewport(section)) {
+//       link.classList.replace("text-green-900", "text-green-500");
+//     } else {
+//       link.classList.replace("text-green-500", "text-green-900");
+//     }
+//   });
 // });
 
-// Initialization for ES Users
-// import {
-//     Collapse,
-//     Ripple,
-//     initTWE,
-//   } from "tw-elements";
-
-//   initTWE({ Collapse, Ripple });
-
-const div = document.querySelector('#navbar-sticky');
-const btnn = document.querySelector('#btn');
-btnn.addEventListener("click", function () {
-    if (div.classList.contains('hidden')) {
-        div.classList.remove('hidden');
-    }
-    else {
-        div.classList.add('hidden');
-    }
-});
+// function isElementInViewport(el) {
+//   const rect = el.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein300: "slidein 1s ease 300ms forwards",
+        slidein500: "slidein 1s ease 500ms forwards",
+        slidein700: "slidein 1s ease 700ms forwards",
+      },
+    },
     fontFamily:{
       Poppins:['Poppins', 'sans-serif']
     },
